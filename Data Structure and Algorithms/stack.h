90bad4c27c394315a73c8ac746fcdbd013ccb6be
_hiimdang <haidanglogicalwitch@gmail.com>
2023-02-21 22:08:50 +0700

    Add Stack Lib
    Thêm thư viện nè Stack nè

---
+++ Data Structure and Algorithms/stack.h
@@ -0,0 +1,50 @@
+using namespace std;
+
+template <class T>
+struct NODES {
+	T data;
+	NODES<T>* pNext;
+};
+template <class T>
+NODES<T>* CreateNodeS(T x) {
+	NODES<T>* p = new NODES<T>;
+	p->data = x;
+	p->pNext = NULL;
+	return p;
+}
+template <class T>
+struct Stack {
+	NODES<T>* pTop;
+
+	void init();
+	void push(T);
+	NODES<T>* pop();
+	NODES<T>* top();
+};
+template <class T>
+void Stack<T>::init() {
+	pTop = NULL;
+}
+template <class T>
+void Stack<T>::push(T x) {
+	NODES<T>* p = CreateNodeS(x);
+	if (p == NULL) return;
+	if (pTop == NULL) {
+		pTop = p;
+	}
+	else {
+		p->pNext = pTop;
+		pTop = p;
+	}
+}
+template <class T>
+NODES<T>* Stack<T>::top() {
+	return pTop;
+}
+template <class T>
+NODES<T>* Stack<T>::pop() {
+	NODES<T>* p = pTop;
+	pTop = p->pNext;
+	p->pNext = NULL;
+	return p;
+}
