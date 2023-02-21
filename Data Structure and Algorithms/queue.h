4fc2269f4e798620d652e9989a5c54104de25f10
_hiimdang <haidanglogicalwitch@gmail.com>
2023-02-21 22:10:26 +0700

    Add Queue Lib
    Thêm thư viện về Queue nè

---
+++ Data Structure and Algorithms/queue.h
@@ -0,0 +1,53 @@
+using namespace std;
+
+template <class T>
+struct NODEQ {
+	T data;
+	NODEQ<T>* pNext;
+};
+template <class T>
+NODEQ<T>* CreateNodeQ(T x) {
+	NODEQ<T>* p = new NODEQ<T>;
+	p->data = x;
+	p->pNext = NULL;
+	return p;
+}
+template <class T>
+struct Queue {
+	NODEQ<T>* pHead;
+	NODEQ<T>* pTail;
+
+	void init();
+	void push(T);
+	NODEQ<T>* pop();
+	NODEQ<T>* top();
+};
+
+template <class T>
+void Queue<T>::init(){
+	pHead = NULL;
+	pTail = NULL;
+}
+template <class T>
+void Queue<T>::push(T x) {
+	NODEQ<T>* p = CreateNodeQ(x); 
+	if (p == NULL) return;
+	if (pHead == NULL) {
+		pHead = pTail = p;
+	}
+	else {
+		pTail->pNext = p;
+		pTail = p;
+	}
+}
+template <class T>
+NODEQ<T>* Queue<T>::top() {
+	return pHead;
+}
+template <class T>
+NODEQ<T>* Queue<T>::pop() {
+	NODEQ<T>* p = pHead;
+	pHead = p->pNext;
+	p->pNext = NULL;
+	return p;
+}
